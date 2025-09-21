504273fc4481b229e66fe632d58072ff3c98b3b9
JohanJvR <u22590732@tuks.co.za>
2025-09-21 22:02:10 +0200

    Updated Pdf 2

--- Calculator/obj/Debug/net9.0/Calculator.AssemblyInfo.cs
+++ Calculator/obj/Debug/net9.0/Calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d84ecf0463b23ff3a5c643bee4d3b6588c7414e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+597ddb7478c3eaf6170c1f5ad3b20f67674f9be8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
